ca90ca3dd99c6588d73b218cb42c80571c989fe4
Michael Dugan <dugan.183@osu.edu>
2019-03-19 18:33:39 -0400

    test commit

--- src/ping_driver.py
+++ src/ping_driver.py
@@ -1,9 +1,22 @@
 from brping import Ping1Dpy
+import serial
+import rospy
+from std_msgs.msg import String, Header
+
+#make new msg?
+
 myPing = Ping1D("/dev/ttyUSB0", 115200)
+
+#corepro
 
 import rospy; 
 
 def talker():
+    rospy.init_node('ping_viewer')
+    dataRead = True
+    #publisher
     pub = rospy.Publisher('/ping/raw'
+
+    if __name__ == "__talker__": talker()
 
 
