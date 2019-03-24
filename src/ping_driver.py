9d0e225d103976c085d85846c96cc5bb1989f563
Anden Acitelli <a.acitelli@gmail.com>
2019-03-24 14:00:56 -0400

    Fixed ROS node initialization issues; Fixed blue robotics ping import library; Other misc. small fixes

--- src/ping_driver.py
+++ src/ping_driver.py
@@ -1,22 +1,35 @@
-from brping import Ping1Dpy
-import serial
-import rospy
-from std_msgs.msg import String, Header
+#!/usr/bin/env python
 
-#make new msg?
+# Issues: 
+# Have to run source devel/setup.bash at every terminal for ROS to recognize the package 
+# Had to chmod the file itself to modify its permissions - Should we have? 
+# How to physically import the message? Any weird ROS stuff? 
 
+# Imports ping packages 
+from brping import Ping1D
+
+# Serial Port & Baud (data transfer rate / sampling rate)
 myPing = Ping1D("/dev/ttyUSB0", 115200)
 
-#corepro
+# Imports ros packages and necessary messages 
+import rospy
+from ping_driver.msg import pingMsg
 
-import rospy; 
+# Initializes the class with ROS and sets up a publisher
+rospy.init_node('ping_viewer')
+pub = rospy.Publisher('/ping/raw', String, queue_size=10)
 
-def talker():
-    rospy.init_node('ping_viewer')
-    dataRead = True
-    #publisher
-    pub = rospy.Publisher('/ping/raw'
+# Initializes this as a node within ros 
+rate = rospy.Rate(10)
 
-    if __name__ == "__talker__": talker()
+outputString = "hello, world!"
+
+while not rospy.is_shutdown():
+
+    # Publishes it onto its constructed topic via ros 
+    pub.publish(outputString)
+
+    # Sleeps according to the Hz given to it 
+    rate.sleep()
 
 
